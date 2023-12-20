b2700553a4e21b3b3580af8c7a3c83931e3562ca
aelyos <thehivemc1234@gmail.com>
2023-12-20 19:19:14 +0100

    Red Slime is better this way

--- src/monster/MON_RedSlime.java
+++ src/monster/MON_RedSlime.java
@@ -62,20 +62,12 @@ public class MON_RedSlime extends Entity{
         Random random = new Random();
         int i  = random.nextInt(100)+1; //it will pick up a number from 0 to 100
         
-        if(i <= 25) {
+        if(i <= 50) {
             direction = "up";
         }
 
-        if(i > 25 && i <= 50) {
+        else {
             direction = "down";
-        }
-
-        if(i > 50 && i <= 75) {
-            direction = "left";
-        }
-
-        if(i > 75 && i <= 100) {
-            direction = "right";        
         }
 
         actionLockCounter = 0;
